53b214a73cb99f161645db06dbc0dea35e49c922
max <max@hardstyleunlimited.com>
2026-08-08 17:16:58 -0500

    subabase change

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,10 @@
+import { createClient } from
+  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
+const SUPABASE_URL = 'https://bqnidckncilclhvbglac.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_NHz0g89ZGi6WqMuSjnmxEw_t2VvOzOd';
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
